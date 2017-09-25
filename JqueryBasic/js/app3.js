c2ad65f66513d0cbca97e7527ff7d66baa989a5b
Jeff Ripke <jripke74@gmail.com>
2017-09-25 08:27:42 -0700

    JqueryBasic: setup for creating a simple lightbox

---
+++ JqueryBasic/js/app3.js
@@ -0,0 +1,15 @@
+ // Prevent spoilerphobes from seeing spoilers
+ // Solution: Hide spoilers and reveal them through user ineraction
+
+ // 1. Hide spoilers
+ $(".spoiler span").hide();
+ // 2. Add a button
+ $(".spoiler").append("<button>Reveal Spoler!</button>");
+ // 3. When Button Pressed
+ $("button").click(function() {
+   // 3.1. Show spoiler next to the button clicked
+   $(this).prev().show();
+   $(".spoiler span").show();
+   // 3.2. Get rid of button
+   $(this).remove();
+ });
