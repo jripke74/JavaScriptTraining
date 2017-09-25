967ac5ed941b2a6dfb185e769327707e75acf341
Jeff Ripke <jripke74@gmail.com>
2017-09-25 05:59:22 -0700

    JqueryBasics: add button in app.js

--- JqueryBasic/js/app.js
+++ JqueryBasic/js/app.js
@@ -2,7 +2,9 @@
  // Solution: Hide spoilers and reveal them through user ineraction
 
  // 1. Hide spoilers
+ $(".spoiler span").hide();
  // 2. Add a button
+ $(".spoiler").append("<button>Reveal Spoler!</button>");
  // 3. When Button Pressed
   // 3.1. Show spoiler
   // 3.2. Get rid of button
