26a0019445851d2b83037f5d9071642fe3a65b87
Jeff Ripke <jripke74@gmail.com>
2017-09-25 05:53:08 -0700

    JqueryBasics: corrected missspelling of spoiler

---
+++ JqueryBasic/js/app.js
@@ -0,0 +1,8 @@
+ // Prevent spoilerphobes from seeing spoilers
+ // Solution: Hide spoilers and reveal them through user ineraction
+
+ // 1. Hide spoilers
+ // 2. Add a button
+ // 3. When Button Pressed
+  // 3.1. Show spoiler
+  // 3.2. Get rid of button
