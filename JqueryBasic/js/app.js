db51e1cc14dd02622b3644c1ffc34a775f6e9dee
Jeff Ripke <jripke74@gmail.com>
2017-09-26 09:34:57 -0700

    JqueryBasic: CreatingAMobileDropDownMenu add drop down menu for smaller screens

---
+++ JqueryBasic/js/app.js
@@ -0,0 +1,28 @@
+// Problem: It looks gross in smaller browser widths and small devices
+// Solution: To hide the text links and swap them out with a more appropriate
+
+// Create a select and append to #menu
+let $select = $("<select></select>");
+$("#menu").append($select);
+
+// Cycle over menu links
+$("#menu a").each(function() {
+  let $anchor = $(this);
+
+  // Create an option
+  let $option = $("<option></option>");
+
+  // option's value is the href
+  $option.val($anchor.attr("href"));
+  // option's text is the text of link
+  $option.text($anchor.text());
+  // append option to select
+  $select.append($option);
+});
+
+// Create button
+// Bind click to button
+  // Go to select's location
+// Modify CSS to hide links on small width and show button and select
+ // Also hides select and button on larger width and show's links
+// Deal with selected options depending on current page
