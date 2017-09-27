85d3f532d496c7a1e5bcae792abcb9fe7fe1765c
Jeff Ripke <jripke74@gmail.com>
2017-09-27 06:23:48 -0700

    JqueryBasic: hide and show password hint

--- JqueryBasic/js/app.js
+++ JqueryBasic/js/app.js
@@ -1,3 +1,24 @@
-// Hide warning
-// Show warning slowly
-$(".warning").hide().show("slow");
+// Problem: Hints are shown even when form is valid
+// Solution: Hide and show them at appropriate times
+
+// Hide Hints
+$("form span").hide();
+
+function passwordEvent() {
+  // Find out if password is valid
+  if ($(this).val().length > 8) {
+    // Hide hint if valid
+    $(this).next().hide();
+  } else {
+    // else show hint
+    $(this).next().show();
+  }
+}
+
+// When event happens on password input
+$("#password").focus(passwordEvent).keyup(passwordEvent);
+
+// When event happens on confirmation input
+  // Find out if password and confirmation match
+  // Hide hint if match
+  // else show hint
